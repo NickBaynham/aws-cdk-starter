64bde9766f8b31b1763b51f064a2ac7f2cd4b14c
Nick Baynham <nickbaynham@gmail.com>
2024-02-17 10:00:48 -0500

    added ecs stack and task

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,9 +1,41 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import {Construct} from 'constructs';
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import {Compatibility, ContainerDefinition, TaskDefinition} from "aws-cdk-lib/aws-ecs";
+import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const vpc = new ec2.Vpc(this, "MyVpc", {
+      maxAzs: 3 // Default is all AZs in region
+    });
+
+    const cluster = new ecs.Cluster(this, "MyCluster", {
+      vpc: vpc
+    });
+
+    const taskDefinition = new TaskDefinition(this, 'task1', {
+      cpu: "512",
+      memoryMiB: "1024",
+      compatibility: Compatibility.FARGATE,
+    });
+
+    taskDefinition.defaultContainer = new ContainerDefinition(this, 'container', {
+      image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
+      taskDefinition: taskDefinition
+    });
+
+    // Create a load-balanced Fargate service and make it public
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService", {
+      cluster: cluster, // Required
+      cpu: 512, // Default is 256
+      desiredCount: 6, // Default is 1
+      taskImageOptions: { image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample") },
+      memoryLimitMiB: 2048, // Default is 512
+      publicLoadBalancer: true // Default is true
+    });
   }
 }
